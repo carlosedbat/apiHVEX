bac4e5c19d598477691460b60b9891d30e5ffe3f
Carlos Eduardo <carlosedbat@gmail.com>
2022-07-19 00:26:05 -0300

    list code http state are defined in category controller

--- src/controllers/category.controller.ts
+++ src/controllers/category.controller.ts
@@ -2,11 +2,21 @@ import {Request, Response} from 'express';
 import { Categories } from '../services/categories.services';
 
 export const categories = async (req:Request,res:Response)=>{
-    let categories = await Categories.findCategories()
-    res.json(categories)
+    try {
+        let categories = await Categories.findCategories()
+        res.status(200).json(categories)
+    } catch (error) {
+        res.status(404).json(error)
+    }
+    
 };
 
 export const allCategories = async (req:Request,res:Response)=>{
-    let categories = await Categories.findAll()
-    res.json(categories)
+    try {
+        let categories = await Categories.findAll()
+        res.json(categories)
+    } catch (error) {
+        res.status(404).json(error)
+    }
+    
 };
